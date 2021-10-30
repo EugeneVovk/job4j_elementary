ffdf1dd50586a12cabe53d20af93778f8ea36ac1
Eugene Vovk <eugenevovk@gmail.com>
2021-10-30 15:02:00 +0300

    4.3.4. if c return [#333785 #222453] deleted a comment

--- src/main/java/ru/job4j/condition/PointEq.java
+++ src/main/java/ru/job4j/condition/PointEq.java
@@ -2,9 +2,6 @@ package ru.job4j.condition;
 
 public class PointEq {
 
-    /**
-     * Метод eq проверяет, что две точки в системе координат одинаковы.
-     */
     public static boolean eg(int x1, int y1, int x2, int y2) {
         return x1 == x2 && y1 == y2;
     }
