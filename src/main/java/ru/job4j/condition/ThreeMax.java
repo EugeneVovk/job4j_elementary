9cf496dc7283e824491515b7a8d3c77c91f0ecb3
Eugene Vovk <eugenevovk@gmail.com>
2021-10-30 20:01:30 +0300

    4.5. Отладка программы в IDEA [#159464]

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -12,9 +12,4 @@ public class ThreeMax {
         }
         return rsl;
     }
-
-    public static void main(String[] args) {
-        int rsl = ThreeMax.max(16, 16, 6);
-        System.out.println(rsl);
-    }
 }
