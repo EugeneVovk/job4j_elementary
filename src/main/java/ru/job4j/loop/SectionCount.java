9462589a32722205fe2a54a9469174bd8740fdea
Eugene Vovk <eugenevovk@gmail.com>
2021-11-01 20:38:28 +0300

    1.2. Найти количество отрезков, используя цикл

---
+++ src/main/java/ru/job4j/loop/SectionCount.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class SectionCount {
+
+    public static int count(int length, int section) {
+        int count = 0;
+        while (length >= section) {
+            length -= section;
+            count++;
+        }
+        return count;
+    }
+}
