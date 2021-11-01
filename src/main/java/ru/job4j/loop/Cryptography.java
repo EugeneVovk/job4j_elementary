af735527b79f1f3e4ce48ac2cc4912d458ddee25
Eugene Vovk <eugenevovk@gmail.com>
2021-11-01 20:45:05 +0300

    1.6. Шифр строки

---
+++ src/main/java/ru/job4j/loop/Cryptography.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Cryptography {
+
+    public static String code(String s) {
+        if (s.length() == 0) {
+            s = "empty";
+        } else {
+            StringBuilder sb = new StringBuilder(s);
+            for (int i = 0; i < s.length() - 4; i++) {
+                sb.setCharAt(i, '#');
+            }
+            s = sb.toString();
+        }
+        return s;
+    }
+}
