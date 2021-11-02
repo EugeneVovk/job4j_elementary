220c703dfacd3a3113d6ac79a0388824c2844a7f
Eugene Vovk <eugenevovk@gmail.com>
2021-11-02 18:38:35 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -31,4 +31,48 @@ public class FindLoopTest {
         int expected = -1;
         Assert.assertEquals(expected, rst);
     }
+
+    @Test
+    public void whenFind3() {
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenNotFind() {
+        int[] data = {1, 2, 3, 4, 5, 6, 7, 8};
+        int el = 3;
+        int start = 3;
+        int finish = data.length - 1;
+        int rst = FindLoop.indexOf(data, el, start, finish);
+        int expected = -1;
+        Assert.assertEquals(expected, rst);
+    }
+
+    @Test
+    public void whenFind0() {
+        int[] data = {8, 56, 1, 2, 0, 0, 99};
+        int el = 8;
+        int start = 0;
+        int finish = data.length / 2;
+        int rst = FindLoop.indexOf(data, el, start, finish);
+        int expected = 0;
+        Assert.assertEquals(expected, rst);
+    }
+
+    @Test
+    public void whenFind4() {
+        int[] data = {8, 0, 0, 2, 0, 0, 99};
+        int el = 0;
+        int start = data.length / 2;
+        int finish = data.length - 1;
+        int rst = FindLoop.indexOf(data, el, start, finish);
+        int expected = 4;
+        Assert.assertEquals(expected, rst);
+    }
 }
