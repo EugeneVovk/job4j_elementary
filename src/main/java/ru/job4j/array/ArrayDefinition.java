11541159c2f4c26b9c9a3ae848d07297d96fcf3a
Eugene Vovk <eugenevovk@gmail.com>
2021-11-02 12:54:46 +0300

    6.0.2. Заполнение массива. [#226300] added the "for" operator

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -20,5 +20,9 @@ public class ArrayDefinition {
         System.out.println(names[1]);
         System.out.println(names[2]);
         System.out.println(names[3]);
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
