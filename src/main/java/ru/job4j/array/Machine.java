4306d04fe6a1b270660770f6679b26bb68cdb882
Eugene Vovk <eugenevovk@gmail.com>
2021-11-04 13:44:36 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #223960] changed the loop for to for-each

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -9,16 +9,12 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int change = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (change >= coins[i]) {
-                change -= coins[i];
-                rsl[size++] = coins[i];
+        for (int coin : coins) {
+            while (change >= coin) {
+                change -= coin;
+                rsl[size++] = coin;
             }
         }
         return Arrays.copyOf(rsl, size);
-    }
-
-    public static void main(String[] args) {
-
     }
 }
